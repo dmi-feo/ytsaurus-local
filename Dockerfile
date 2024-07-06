e8d5d1776d95333d1d853f5d9d892b2dad818fd5
Dmitry Fedorov <fdi1992@gmail.com>
2024-07-06 19:20:43 +0200

    auth

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-FROM ghcr.io/ytsaurus/ytsaurus-nightly:dev-2024-07-03-c05f760b02d9610bc5f2767e8dfa458c9c66bbd7 as ytsaurus
-
+FROM ghcr.io/ytsaurus/ytsaurus:stable-23.2.0 as ytsaurus
 FROM ubuntu:22.04
 
 RUN apt-get update && apt-get install -y supervisor python3 python3-pip && apt-get clean
-# RUN pip install ytsaurus-client
 
 COPY --from=ytsaurus /usr/bin/ytserver-all /usr/bin/ytserver-all
 
@@ -17,8 +15,6 @@ RUN for package in client yson local native_driver; \
 
 
 COPY ./configs /configs
-COPY ./init.sh /configs
-COPY ./client.yson /configs
 COPY ./supervisord.conf /etc/supervisord.conf
 
 RUN for SERVICE in master http-proxy node scheduler controller-agent; \
--- configs/init.sh
+++ configs/init.sh
@@ -24,12 +24,15 @@ yt set //sys/schemas/access_control_object/@acl '[{action=allow;subjects=[users;
 yt set //sys/schemas/account_resource_usage_lease/@acl '[{action=allow;subjects=[users;];permissions=[read;write;create;];};{action=allow;subjects=[admins;];permissions=[read;write;administer;create;remove;];};]'
 
 yt create scheduler_pool_tree --attributes '{name=default; config={nodes_filter=""}}' --ignore-existing
+
 if [ $(yt exists //sys/pool_trees/@default_tree) = 'false' ]; then
-yt set //sys/pool_trees/@default_tree default
+  yt set //sys/pool_trees/@default_tree default
 fi
+
 if [ $(yt exists //sys/pools) = 'false' ]; then
-yt link //sys/pool_trees/default //sys/pools
+  yt link //sys/pool_trees/default //sys/pools
 fi
+
 yt create scheduler_pool --attributes '{name=research; pool_tree=default}' --ignore-existing
 yt create map_node //home --ignore-existing
 
@@ -62,13 +65,15 @@ yt set //sys/@cluster_connection '{
 }'
 
 if [ $(yt exists //sys/controller_agents/config/operation_options/spec_template) = 'false' ]; then
-yt set //sys/controller_agents/config/operation_options/spec_template '{enable_partitioned_data_balancing=%false}' -r
+  yt set //sys/controller_agents/config/operation_options/spec_template '{enable_partitioned_data_balancing=%false}' -r
 fi
 
 if [ $(yt exists //sys/users/admin) = 'false' ]; then
-yt create user --attributes '{name="admin"}'
-yt execute set_user_password '{user=admin;new_password_sha256="3a5745a05f87ddee1db68b217dc043bfa206d1c7aaa1dd0a7dd76b852a733597"}'
-yt create map_node '//sys/cypress_tokens/87a5d9406ccf6a42cca510d86e43b20e2943aa7ade7e9129f4f4f947e1b02574' --ignore-existing
-yt set '//sys/cypress_tokens/87a5d9406ccf6a42cca510d86e43b20e2943aa7ade7e9129f4f4f947e1b02574/@user' 'admin'
-yt add-member admin superusers
+  yt create user --attributes '{name="admin"}'
+  yt execute set_user_password '{user=admin;new_password_sha256="53336a676c64c1396553b2b7c92f38126768827c93b64d9142069c10eda7a721"}'
+  yt create map_node '//sys/cypress_tokens/53336a676c64c1396553b2b7c92f38126768827c93b64d9142069c10eda7a721' --ignore-existing
+  yt set '//sys/cypress_tokens/53336a676c64c1396553b2b7c92f38126768827c93b64d9142069c10eda7a721/@user' 'admin'
+  yt add-member admin superusers
 fi
+
+yt create document //sys/client_config --attributes '{"value"={"proxy"={"enable_proxy_discovery"=%false};};}'
