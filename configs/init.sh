01835b5d1633e3664a9ef67eb001ac9fc6d6bf6d
Dmitry Fedorov <fdi1992@gmail.com>
2024-07-20 15:45:10 +0200

    some fixes

--- configs/init.sh
+++ configs/init.sh
@@ -3,16 +3,16 @@
 set -ex
 
 # cgroup v2: enable nesting
-#if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-#	# move the processes from the root group to the /init group,
-#	# otherwise writing subtree_control fails with EBUSY.
-#	# An error during moving non-existent process (i.e., "cat") is ignored.
-#	mkdir -p /sys/fs/cgroup/init
-#	xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
-#	# enable controllers
-#	sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
-#		> /sys/fs/cgroup/cgroup.subtree_control
-#fi
+if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
+	# move the processes from the root group to the /init group,
+	# otherwise writing subtree_control fails with EBUSY.
+	# An error during moving non-existent process (i.e., "cat") is ignored.
+	mkdir -p /sys/fs/cgroup/init
+	xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+	# enable controllers
+	sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
+		> /sys/fs/cgroup/cgroup.subtree_control
+fi
 
 export YT_DRIVER_CONFIG_PATH=/configs/client.yson
 
